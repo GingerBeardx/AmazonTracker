1644c7210ac66aef17ad8a10822f9ad702ef1b50
GingerBeardx <ecryptographer@gmail.com>
2020-01-11 17:17:06 -0700

    Created user registration functionality

--- sqlconnection.py
+++ sqlconnection.py
@@ -1,4 +1,4 @@
-#! /bin/python37
+# python37
 """
 A console app that tracks amazon prices and sends email notificatinos of drops in prices in order to
 facilitiate smart purchases
@@ -12,6 +12,8 @@ import mysql.connector
 
 #native module imports
 import os
+
+num_hyphens = 90
 
 class dbConnect():
     """ Establishes a connection to the database and facilitates database interactions """
@@ -29,6 +31,7 @@ class dbConnect():
         self.cnx = mysql.connector.connect(user=config['user'], password=config['password'], host=config['host'], database=config['database'], port=config['port'])
         
 def login_user(email, password, user_verified):
+    """ Logs in a new user to the database once valid login credentials have been supplied """
     cnx = dbConnect().cnx
     cursor = cnx.cursor()
     query = (f'''SELECT email, passwd, first_name FROM users WHERE email="{email}";''') 
@@ -41,11 +44,49 @@ def login_user(email, password, user_verified):
         if r[1] == password:
             name = r[2]
             user_verified = True
+    cursor.close()
+    cnx.close()
     return user_verified, name
+    
+def register_user():
+    """ Creates a new user as long as the user supplies an unique email address """
+    unique_email = False
+    print("Thank you for registering. Please enter the following:")
+    print("-" * num_hyphens)
+    first_name = input("Please enter your first name -> ")
+    last_name = input("Please enter your last name -> ")
+    email = input("Please enter your email -> ")
+    
+    cnx = dbConnect().cnx
+    cursor = cnx.cursor()
+    
+    while not unique_email:
+        query = (f'''SELECT email FROM users WHERE email="{email}";''') 
+        
+        data = email
+        cursor.execute(query)
+
+        result = [r for r in cursor]                            
+        
+        if len(result) > 0:
+            print("I'm sorry, that is not a valid email.")
+            email = input("Please enter your email -> ")
+        else:
+            unique_email = True
+        
+    password = input("Please enter your password -> ")
+        
+    query = f'''INSERT INTO users (first_name, last_name, email, passwd)
+                VALUES ("{first_name}", "{last_name}", "{email}", "{password}");'''
+    cursor.execute(query)
+    cnx.commit()
+    cursor.close()
+    cnx.close()
+    input(f"A new user has been created succesfully! Welcome, {first_name}! -> ")
 
 def main():
     """ This is the main program loop that will run when this file is executed """
-    num_hyphens = 90
+    
     running = True
     while running:
         os.system('cls' if os.name == 'nt' else 'clear')
@@ -62,7 +103,7 @@ def main():
         print("-" * num_hyphens)
         
         if int(choice) == 1:
-            pass
+            register_user()
         elif int(choice) == 2:
             user_verified = False
             print("You are logging in. Please enter your login info:")
